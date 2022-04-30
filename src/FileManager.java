2c7b7f51f6f9b6a3c9953c5e888b54cb327eae80
s777ef <103335856+s777ef@users.noreply.github.com>
2022-04-30 15:08:36 -0300

    Add `FileManager` class

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,3 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class FileManager {
-    
+    private final String FILENAME;
+
+    public FileManager(String filename) {
+        this.FILENAME = filename;
+    }
+
+    public ArrayList<BigInteger> read() {
+        ArrayList<BigInteger> numbers = new ArrayList<BigInteger>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                numbers.add(new BigInteger(line.split(",")[0]));
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numbers;
+    }
+
+    public void write(ArrayList<BigInteger> numbers) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME));
+
+            for (int i = 0; i < numbers.size(); ++i) {
+                writer.write(numbers.get(i) + "\n");
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
