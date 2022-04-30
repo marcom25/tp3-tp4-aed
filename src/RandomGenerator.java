ace875d9d1cd3de5c5338a54fad331e73e1e8020
Sebastián Martin Ponce <sebaottok@gmail.com>
2022-04-30 00:34:35 -0300

    update: methods working

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -2,18 +2,43 @@ import java.math.BigInteger;
 
 public class RandomGenerator {
 
-    public static BigInteger vonNewman(BigInteger seed) {
+    public void vonNewman(BigInteger seed, int data) {
+        int count = 0;
+        String subString;
+        String multipliedText;
 
-        return null;
+        for (int i = 0; i < data; i++) {
+            seed = seed.multiply(seed);
+
+            multipliedText = String.valueOf(seed);
+            count = multipliedText.length();
+
+            if (count == 20) {
+                subString = multipliedText.substring(5,15);    
+            } else {	
+                subString = multipliedText.substring(4,14);
+            }
+            
+            seed = BigInteger.valueOf(Long.parseLong(subString));
+
+            System.out.println("Seed multiplied "+ (i+1) + ": " + multipliedText);
+            System.out.println("Seed "+ (i+1) + ": " + seed);
+            System.out.println();
+        }
     }
 
-    public static BigInteger congruential(BigInteger seed) {
+
+
+    public void congruential(BigInteger seed, int data) {
         BigInteger a = new BigInteger("1103515245");
         BigInteger c = new BigInteger("12345");
         BigInteger m = new BigInteger("" + (int) Math.pow(2, 32));
         
-        return seed = (seed.multiply(a).add(c)).mod(m);
-
+        for (int i = 0; i < data; i++) {
+            seed = (seed.multiply(a).add(c)).mod(m);
+            System.out.println("Seed: " + seed);
+            System.out.println();
+        }
         
     }
     
